864e95e36b6737b3aaaffda3974c066b828dd453
elina0524 <elina0524@gmail.com>
2016-12-03 00:21:32 -0500

    stomp boss

--- The GREat Escape/Assets/Scripts/StompEnemy.cs
+++ The GREat Escape/Assets/Scripts/StompEnemy.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 
 public class StompEnemy : MonoBehaviour {
 	// Use this for initialization
+	public Text wordDisplay;
+
 	void Start () {
 	
 	}
@@ -16,6 +18,10 @@ public class StompEnemy : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Enemy") {
 			Destroy (other.gameObject);
+		}
+
+		if (other.tag == "Boss") {
+			wordDisplay.text = "Multiple Choice";
 		}
 
 	}
