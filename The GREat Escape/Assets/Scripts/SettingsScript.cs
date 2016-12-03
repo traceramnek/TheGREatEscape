3b80b99610fbaa742ec1216d844e05df97c4ac57
Kwan Holloway <kwanholloway@newdesignhigh.com>
2016-12-02 23:49:33 -0500

    Settings Update

---
+++ The GREat Escape/Assets/Scripts/SettingsScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SettingsScript : MonoBehaviour {
+
+	public Slider volSlider;
+
+	// Use this for initialization
+	void Start () {
+		volSlider = GetComponent<Slider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void adjustVolume(){
+		AudioListener.volume = volSlider.value / 10.0f;
+	}
+
+	public void toggleFullscreen(){
+		Screen.fullScreen = !Screen.fullScreen;
+	}
+}
