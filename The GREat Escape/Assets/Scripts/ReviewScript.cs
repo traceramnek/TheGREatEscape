b31cf952b0fe12d910219fa9b0f76ca6eef35b98
Kwan Holloway <kwanholloway@newdesignhigh.com>
2016-12-10 13:05:07 -0500

    MISC Commit

--- The GREat Escape/Assets/Scripts/ReviewScript.cs
+++ The GREat Escape/Assets/Scripts/ReviewScript.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class ReviewScript : MonoBehaviour {
 	
@@ -15,23 +16,23 @@ public class ReviewScript : MonoBehaviour {
 
 		reviewTitle.text = "Word Review";
 
-		switch (reviewNum) {
-		case 1:	
-			revWord1.text = BookScript.bookControl.reviewWords [0];
-			revWord2.text = BookScript.bookControl.reviewWords [1];
-			revWord3.text = BookScript.bookControl.reviewWords [2];
-			revWord4.text = BookScript.bookControl.reviewWords [3];
-			break;
-		case 2:	
-			revWord1.text = BookScript.bookControl.reviewWords [4];
-			revWord2.text = BookScript.bookControl.reviewWords [5];
-			revWord3.text = BookScript.bookControl.reviewWords [6];
-			revWord4.text = BookScript.bookControl.reviewWords [7];
-			break;
-		default:
-			// nothing
-			break;
-		}
+//		switch (reviewNum) {
+//		case 1:	
+//			revWord1.text = BookScript.bookControl.reviewWords [0];
+//			revWord2.text = BookScript.bookControl.reviewWords [1];
+//			revWord3.text = BookScript.bookControl.reviewWords [2];
+//			revWord4.text = BookScript.bookControl.reviewWords [3];
+//			break;
+//		case 2:	
+//			revWord1.text = BookScript.bookControl.reviewWords [4];
+//			revWord2.text = BookScript.bookControl.reviewWords [5];
+//			revWord3.text = BookScript.bookControl.reviewWords [6];
+//			revWord4.text = BookScript.bookControl.reviewWords [7];
+//			break;
+//		default:
+//			// nothing
+//			break;
+//		}
 
 
 		//setReviewWords ();
@@ -64,6 +65,9 @@ public class ReviewScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (SceneManager.GetActiveScene ().name == "Review1") {
+			setReviewWords ();
+		}
+
 	}
 }
