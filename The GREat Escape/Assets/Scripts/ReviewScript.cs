e7fa3438e655d492d57c3c525f6256fa09445b09
Kwan Holloway <kwanholloway@newdesignhigh.com>
2016-12-11 18:18:42 -0500

    ReviewFix

--- The GREat Escape/Assets/Scripts/ReviewScript.cs
+++ The GREat Escape/Assets/Scripts/ReviewScript.cs
@@ -9,31 +9,11 @@ public class ReviewScript : MonoBehaviour {
 	const int NUM_REVIEW_WORDS = 4;
 	public static int reviewNum = 0;
 	public int canSetRev = 2; //used to mod
-	public Text revWord1, revWord2, revWord3, revWord4, reviewTitle;
+	public Text revWord1, revWord2, revWord3, revWord4;
 	//public Text [] reviewwords;
 
 	// Use this for initialization
 	void Start () {
-		//updateReviewNum ();
-		reviewTitle.text = "Word Review";
-
-//		switch (reviewNum) {
-//		case 1:	
-//			revWord1.text = BookScript.bookControl.reviewWords [0];
-//			revWord2.text = BookScript.bookControl.reviewWords [1];
-//			revWord3.text = BookScript.bookControl.reviewWords [2];
-//			revWord4.text = BookScript.bookControl.reviewWords [3];
-//			break;
-//		case 2:	
-//			revWord1.text = BookScript.bookControl.reviewWords [4];
-//			revWord2.text = BookScript.bookControl.reviewWords [5];
-//			revWord3.text = BookScript.bookControl.reviewWords [6];
-//			revWord4.text = BookScript.bookControl.reviewWords [7];
-//			break;
-//		default:
-//			// nothing
-//			break;
-//		}
 
 		updateReviewNum ();
 		//setReviewWords ();
